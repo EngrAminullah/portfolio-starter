ccd659bfa19d09371a650ffd2a9eee5544db7cf5
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-05-30 19:02:08 +0500

    nav style

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div className="n-wrapper">
         <div className="n-left">
-            <div className="n-name">Amin</div>
+            <div className="n-name">Aminew</div>
             <span>Toggle</span>
         </div>
         <div className="n-right">
             <div className="n-list">
-                <ul>
-                    <li>
-                        Home
-                    </li>
+                <ul style={{listStyleType: 'none'}}>
+                    <li>Home</li>
                     <li>Services</li>
                     <li>Experience</li>
                     <li>Portfolio</li>
