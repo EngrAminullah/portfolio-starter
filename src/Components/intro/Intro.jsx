d9112f1e11c40a43c2443de92a27c66b03010ea8
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-05-31 15:06:14 +0500

    1st web screen

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -3,6 +3,13 @@ import './Intro.css'
 import Github from '../../img/github.png'
 import LinkedIn from '../../img/linkedin.png'
 import Instagram from '../../img/instagram.png'
+import Vector1 from '../../img/Vector1.png'
+import Vector2 from '../../img/Vector2.png'
+import boy from '../../img/boy.png'
+import thumbup from '../../img/thumbup.png'
+import Crown from '../../img/crown.png'
+import glassesimoji from '../../img/glassesimoji.png'
+
 const Intro = () => {
   return (
     <div className='intro'>
@@ -20,7 +27,12 @@ const Intro = () => {
             </div>
         </div>
         <div className="i-right">
-            This is right side
+            <img src={Vector1} alt='' />
+            <img src={Vector2} alt='' />
+            <img src={boy} alt=''/>
+            <div>
+                <FloatingDiv/>
+            </div>
         </div>
     </div>
   )
