d8a6c66ae0cc7034b5fd72da7b8cda97d46b5cd6
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-05-31 17:46:37 +0500

    abc

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -9,6 +9,7 @@ import boy from '../../img/boy.png'
 import thumbup from '../../img/thumbup.png'
 import Crown from '../../img/crown.png'
 import glassesimoji from '../../img/glassesimoji.png'
+import FloatingDiv from '../FloatingDiv/FloatingDiv'
 
 const Intro = () => {
   return (
@@ -30,8 +31,11 @@ const Intro = () => {
             <img src={Vector1} alt='' />
             <img src={Vector2} alt='' />
             <img src={boy} alt=''/>
-            <div>
-                <FloatingDiv/>
+            <div style={{top:'-4%', left:'68%'}}>
+                <FloatingDiv image={Crown} txt1='Web' txt2='Developer'/>
+            </div>
+            <div style={{top:'18rem', left:'0rem'}}>
+                <FloatingDiv image={thumbup} txt1='Best Design' txt2='Award'/>
             </div>
         </div>
     </div>
